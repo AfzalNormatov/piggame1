30ee9f40352ecef23a090d603f10210407714f3e
AfzalNormatov <91549976+AfzalNormatov@users.noreply.github.com>
2021-10-01 18:45:25 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,11 @@
-// let scores = [0, 0];
-// let roundScore = 0;
-// let activePlayer = 0;    // src = 'dice-' + dice  => dice-4
-//
-//
-//
-// // document.getElementById('name')
-// // document.getElementsByClassName('name')
-// // document.querySelector('#name')
-// // document.querySelector('.name')
-// // document.querySelector('h1')
-// // document.querySelector('#current--0').textContent = dice;
-// // document.querySelector('#current--0').innerHTML = '<b
-// // >' + dice + '</b>';
-//
-// let diceDom = document.querySelector('.dice');
-// diceDom.style.display = 'none';
-// document.getElementById('score--0').textContent = 0;
-// document.getElementById('score--1').textContent = 0;
-// document.getElementById('current--0').textContent = 0;
-// document.getElementById('current--1').textContent = 0;
-//
-// document.querySelector('.btn--roll').addEventListener('click', function (){
-//     // 1 random number
-//
-//     let dice = Math.floor(Math.random()* 6)+ 1;      // console.log(dice)
-//
-//     // 2 display the results
-//     diceDom.style.display = 'block';
-//     diceDom.src = 'dice-' + dice + '.png';
-//
-//     //3 update the round
-//
-//     if (dice !== 1){
-//         roundScore += dice;
-//
-//         document.querySelector('#current--' + activePlayer).textContent = roundScore;
-//     }
-//
-//     else {
-//         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; // ternary operator
-//         roundScore = 0;
-//         document.getElementById('current--0').textContent = 0;
-//         document.getElementById('current--1').textContent = 0;
-//
-//         document.querySelector('.player--0').classList.toggle('player--active');
-//         document.querySelector('.player--1').classList.toggle('player--active');
-//         // document.querySelector('.player--1').classList.remove('player--active'); faqat 1marta ishliydi
-//         // document.querySelector('.player--0').classList.add('player--active');
-//
-//         diceDom.style.display = 'none';
-//     }
-// })
+
 
 
 
 let scores = [0, 0];
 let roundScore = 0;
 let activePlayer = 0;
-let winScore= 30;
+
 let diceDom = document.querySelector('.dice');
 diceDom.style.display = 'none';
 document.getElementById('score--0').textContent = 0;
@@ -93,9 +41,9 @@ document.querySelector('.btn--hold').addEventListener('click', function(){
     //  Update the UI
     document.querySelector('#score--' + activePlayer).textContent = scores[activePlayer];
     // Check if the player won
-    if(scores[activePlayer] >= winScore){
+    if(scores[activePlayer] >= 20){
 
-        document.querySelector('#name--' + activePlayer).textContent= "WINNER!";
+        document.querySelector('#name--' + activePlayer).textContent= "WINNER !";
         diceDom.style.display= 'none';
         document.querySelector('.player--' + activePlayer).classList.add('player--winner');
         document.querySelector('.player--' + activePlayer).classList.remove('player--active');
